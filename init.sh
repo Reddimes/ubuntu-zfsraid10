f58ee98a262e21a6c1734a32dc53d33fd4f9a089
Jeff Young <jeffyoung1990@gmail.com>
2024-11-29 13:01:42 -0700

    Major change to error handling and needs to be tested

--- init.sh
+++ init.sh
@@ -1,136 +1,222 @@
 #!/bin/bash
 
-# Install what is necessary to run our configuration on the live cd.
-apt install debootstrap gdisk zfsutils-linux -y
+# Function to handle errors
+error_handler() {
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo -e "\e[31mThe script exited with status ${exit_code}.\e[0m" 1>&2
+        cleanup
+        exit ${exit_code}
+    fi
+}
 
-# Wipe disk and repartition the HDDs
-wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X
-wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7
-wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG
-sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X
-sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7
-sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG
-sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X
-sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7
-sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG
-sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X
-sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7
-sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG
-sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X
-sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7
-sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG
+trap error_handler EXIT
 
-# Sleep to allow time for partitions to be detected by zpool
-# Ten seconds is an arbitrary number I used to delay it, it could easily be shorter
-# and it was an arbitrary guess meant to make it work the first time.
-echo "Sleeping..."
-sleep 10
+# Function to run commands and capture stderr
+run_cmd() {
+    local cmd="$1"
+    local stderr_file=$(mktemp)
 
-# Create two zpools, one for /boot, and one for the root directory(/)
-zpool create \
--o ashift=12 \
--o autotrim=on \
--o compatibility=grub2 \
--o cachefile=/etc/zfs/zpool.cache \
--O devices=off \
--O acltype=posixacl -O xattr=sa \
--O compression=lz4 \
--O normalization=formD \
--O relatime=on \
--O canmount=off -O mountpoint=/boot -R /mnt \
-bpool \
-mirror \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK-part2 \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X-part2 \
-mirror \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG-part2 \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7-part2 -f
-zpool create \
--o ashift=12 \
--o autotrim=on \
--O acltype=posixacl -O xattr=sa -O dnodesize=auto \
--O compression=lz4 \
--O normalization=formD \
--O relatime=on \
--O canmount=off -O mountpoint=/ -R /mnt \
-rpool \
-mirror \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK-part3 \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X-part3 \
-mirror \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG-part3 \
-/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7-part3 -f
+    if ! eval "$cmd" > /dev/null 2>$stderr_file; then
+        echo -e "\e[31mError\n Command '$cmd' failed with output:\e[0m" 1>&2
+        cat $stderr_file | awk '{print " \033[31m" $0 "\033[0m"}' 1>&2
+        rm -f $stderr_file
+        exit 1
+    fi
 
-# zpool configuration
-zfs create -o canmount=off -o mountpoint=none rpool/ROOT
-zfs create -o canmount=off -o mountpoint=none bpool/BOOT
-zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
-# Mount pool 
-zfs mount rpool/ROOT/ubuntu
-zfs create -o mountpoint=/boot bpool/BOOT/ubuntu
-## This creates a few extra mounts to isolate data from the rest of linux.
-## Take a look at whether you need them or not.
-zfs create                     rpool/home
-zfs create -o mountpoint=/root rpool/home/root
-# Set permissions for root to be only intended for root.
-chmod 700 /mnt/root
-zfs create -o canmount=off     rpool/var
-zfs create -o canmount=off     rpool/var/lib
-zfs create                     rpool/var/log
-zfs create                     rpool/var/spool
-## Disable snapshotting on the cache, nfs, and tmp.
-zfs create -o com.sun:auto-snapshot=false rpool/var/cache
-zfs create -o com.sun:auto-snapshot=false rpool/var/lib/nfs
-zfs create -o com.sun:auto-snapshot=false rpool/var/tmp
-## Set permissions for tmp
-chmod 1777 /mnt/var/tmp
-## Unecessary if you do not use spool
-zfs create rpool/srv
-## Create dataset for /usr and /usr/local
-zfs create -o canmount=off rpool/usr
-zfs create                 rpool/usr/local
-## Create dataset for /tmp and set it to not snapshot.
-zfs create -o com.sun:auto-snapshot=false  rpool/tmp
-## Set /tmp ppermissions
-chmod 1777 /mnt/tmp
+    rm -f $stderr_file
+}
 
-# I need to read more on why this is a part of the process.
-mkdir /mnt/run
-mount -t tmpfs tmpfs /mnt/run
-mkdir /mnt/run/lock
+# Function to print OK message in green
+print_ok () {
+    echo -e "\e[32mOK\e[0m"
+}
 
-# Use debootstrap to install ubuntu on the hard drive,
-debootstrap noble /mnt http://archive.ubuntu.com/ubuntu
+prerequisites () {
+	echo -n "Installing prerequisites..."
+	run_cmd "apt install debootstrap gdisk zfsutils-linux -y"
+	print_ok
+}
 
-# Copy zpool cache to mounted installation.
-mkdir /mnt/etc/zfs
-cp /etc/zfs/zpool.cache /mnt/etc/zfs/
+partition () {
+	echo -n "Wiping filesystems..."
+	run_cmd "wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK"
+	run_cmd "wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X"
+	run_cmd "wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7"
+	run_cmd "wipefs -a /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG"
+	print_ok
 
-# Networking setup
-hostname cloudstack
-hostname | sudo tee /mnt/etc/hostname
-cp ./hosts /mnt/etc/hosts
+	echo -n "Zapping partitions..."
+	run_cmd "sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK"
+	run_cmd "sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X"
+	run_cmd "sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7"
+	run_cmd "sgdisk --zap-all /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG"
+	print_ok
 
-# Copy over apt configuration and keyrings.  Remove sources.list if it even exists.  I'm not sure.
-cp ./ubuntu.sources /mnt/etc/apt/sources.list.d/ubuntu.sources
-cp /usr/share/keyrings/ubuntu-archive-keyring.gpg /mnt/usr/share/keyrings/ubuntu-archive-keyring.gpg
-rm -f /mnt/etc/apt/sources.list
+	echo -n "Creating partitions..."
+	run_cmd "sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK"
+	run_cmd "sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X"
+	run_cmd "sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7"
+	run_cmd "sgdisk     -n1:1M:+512M   -t1:EF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG"
+	run_cmd "sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK"
+	run_cmd "sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X"
+	run_cmd "sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7"
+	run_cmd "sgdisk     -n2:0:+1G      -t2:BE00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG"
+	run_cmd "sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK"
+	run_cmd "sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X"
+	run_cmd "sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7"
+	run_cmd "sgdisk     -n3:0:0        -t3:BF00 /dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG"
+	print_ok
 
-# Copy over zpool import service which accounts for pre and post execution.
-cp ./zfs-import-bpool.service /mnt/etc/systemd/system/zfs-import-bpool.service
+	# Sleep to allow time for partitions to be detected by zpool
+	# Ten seconds is an arbitrary number I used to delay it, it could easily be shorter
+	# and it was an arbitrary guess meant to make it work the first time.
+	echo -n "Waiting for partitions..."
+	sleep 10
+	print_ok
+}
 
-# Set disk for chroot.
-DISK=/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
-## This section needs more research.
-mount --make-private --rbind /dev  /mnt/dev
-mount --make-private --rbind /proc /mnt/proc
-mount --make-private --rbind /sys  /mnt/sys
+createzpools () {
+	echo -n "Create two zpools, one for /boot, and one for the root directory(/)..."
+	run_cmd "
+	zpool create \
+		-o ashift=12 \
+		-o autotrim=on \
+		-o compatibility=grub2 \
+		-o cachefile=/etc/zfs/zpool.cache \
+		-O devices=off \
+		-O acltype=posixacl -O xattr=sa \
+		-O compression=lz4 \
+		-O normalization=formD \
+		-O relatime=on \
+		-O canmount=off -O mountpoint=/boot -R /mnt \
+			bpool \
+				mirror \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK-part2 \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X-part2 \
+				mirror \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG-part2 \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7-part2 -f"
+	
+	run_cmd "
+	zpool create \
+		-o ashift=12 \
+		-o autotrim=on \
+		-O acltype=posixacl -O xattr=sa -O dnodesize=auto \
+		-O compression=lz4 \
+		-O normalization=formD \
+		-O relatime=on \
+		-O canmount=off -O mountpoint=/ -R /mnt \
+			rpool \
+				mirror \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK-part3 \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV42Y5X-part3 \
+				mirror \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4Q8GG-part3 \
+					/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV4HRM7-part3 -f"
+	print_ok
 
-# Copy over chroot.sh and run it in the chroot environment.
-cp ./chroot.sh /mnt/chroot.sh
-chroot /mnt /usr/bin/env DISK=$DISK bash --login  #-c "/chroot.sh" --login
+	echo -n "Configuring zpools..."
+
+	# zpool configuration
+	run_cmd "zfs create -o canmount=off -o mountpoint=none rpool/ROOT"
+	run_cmd "zfs create -o canmount=off -o mountpoint=none bpool/BOOT"
+	run_cmd "zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu"
+
+	# Mount pool 
+	run_cmd "zfs mount rpool/ROOT/ubuntu"
+	run_cmd "zfs create -o mountpoint=/boot bpool/BOOT/ubuntu"
+
+	## This creates a few extra mounts to isolate data from the rest of linux.
+	## Take a look at whether you need them or not.
+	run_cmd "zfs create                     rpool/home"
+	run_cmd "zfs create -o mountpoint=/root rpool/home/root"
+
+	# Set permissions for root to be only intended for root.
+	run_cmd "chmod 700 /mnt/root"
+	run_cmd "zfs create -o canmount=off     rpool/var"
+	run_cmd "zfs create -o canmount=off     rpool/var/lib"
+	run_cmd "zfs create                     rpool/var/log"
+	run_cmd "zfs create                     rpool/var/spool"
+	
+	## Disable snapshotting on the cache, nfs, and tmp.
+	run_cmd "zfs create -o com.sun:auto-snapshot=false rpool/var/cache"
+	run_cmd "zfs create -o com.sun:auto-snapshot=false rpool/var/lib/nfs"
+	run_cmd "zfs create -o com.sun:auto-snapshot=false rpool/var/tmp"
+	
+	## Set permissions for tmp
+	run_cmd "chmod 1777 /mnt/var/tmp"
+	
+	## Unecessary if you do not use spool
+	run_cmd "zfs create rpool/srv"
+	
+	## Create dataset for /usr and /usr/local
+	run_cmd "zfs create -o canmount=off rpool/usr"
+	run_cmd "zfs create                 rpool/usr/local"
+	
+	## Create dataset for /tmp and set it to not snapshot.
+	run_cmd "zfs create -o com.sun:auto-snapshot=false  rpool/tmp"
+	
+	## Set /tmp permissions
+	run_cmd "chmod 1777 /mnt/tmp"
+	
+	# I need to read more on why this is a part of the process.
+	run_cmd "mkdir /mnt/run"
+	run_cmd "mount -t tmpfs tmpfs /mnt/run"
+	run_cmd "mkdir /mnt/run/lock"
+	print_ok
+}
+
+install () {
+	echo -n "Use debootstrap to install ubuntu on the hard drive..."
+	run_cmd "debootstrap noble /mnt http://archive.ubuntu.com/ubuntu"
+	print_ok
+
+	echo -n "Copying over files..."
+	# Copy zpool cache to mounted installation.
+	run_cmd "mkdir /mnt/etc/zfs"
+	run_cmd "cp /etc/zfs/zpool.cache /mnt/etc/zfs/"
+
+	# Networking setup
+	run_cmd "echo cloudstack | sudo tee /mnt/etc/hostname"
+	run_cmd "cp ./hosts /mnt/etc/hosts"
+
+	# Copy over apt configuration and keyrings.  Remove sources.list if it even exists.  I'm not sure.
+	run_cmd "cp ./ubuntu.sources /mnt/etc/apt/sources.list.d/ubuntu.sources"
+	run_cmd "cp /usr/share/keyrings/ubuntu-archive-keyring.gpg /mnt/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+	run_cmd "rm -f /mnt/etc/apt/sources.list"
+
+	# Copy over zpool import service which accounts for pre and post execution.
+	run_cmd "cp ./zfs-import-bpool.service /mnt/etc/systemd/system/zfs-import-bpool.service"
+	
+	# Copy over script to be run in chroot
+	run_cmd "cp ./chroot.sh /mnt/chroot.sh"
+	
+	# Copy over grub configuration.
+	########## Needs to be setup
+	
+	print_ok
+}
+
+prepareChroot () {
+	# Set disk for chroot. This is temporary.
+	DISK=/dev/disk/by-id/ata-ST12000VN0007-2GS116_ZJV58DGK
+	## This section needs more research.
+	run_cmd "mount --make-private --rbind /dev  /mnt/dev"
+	run_cmd "mount --make-private --rbind /proc /mnt/proc"
+	run_cmd "mount --make-private --rbind /sys  /mnt/sys"
+}
+
+runChroot () {
+	echo "Running chroot..."
+	run_cmd "chroot /mnt /usr/bin/env DISK=$DISK bash --login  #-c \"/chroot.sh\" --login"
+	echo "chroot complete"
+}
+
+# Main Script Execution
+prerequisites
+partition
+createzpools
+install
+prepareChroot
+runChroot
