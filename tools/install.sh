965e2cee62b64985753f257c0d912794ac72c5fa
Jeff Young <jeffyoung1990@gmail.com>
2024-11-30 20:12:41 -0700

    More debugging

--- tools/install.sh
+++ tools/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-TEMPDIR=$(mktemp -d)
-echo $TEMPDIR
-cd $TEMPDIR
+TDIR=$(mktemp -d)
+echo $TDIR
+echo $(pwd)
+cd $TDIR
+echo $(pwd)
 git clone --depth 1 https://github.com/Reddimes/ubuntu-zfsraid10.git ./
 chmod +x *.sh
 sudo ./init.sh
