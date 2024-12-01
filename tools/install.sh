bab1344bb61c8620a33314f6e7f1fc8e1902ec96
Jeff Young <jeffyoung1990@gmail.com>
2024-11-30 21:42:34 -0700

    Remove Temporary Directory as it is far slower.

--- tools/install.sh
+++ tools/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-TDIR=$(mktemp -d)
-cd $TDIR
-git clone --depth 1 https://github.com/Reddimes/ubuntu-zfsraid10.git ./ &> /dev/null
+git clone --depth 1 https://github.com/Reddimes/ubuntu-zfsraid10.git &> /dev/null
+cd ubuntu-zfsraid10
 chmod +x *.sh
 sudo ./init.sh
